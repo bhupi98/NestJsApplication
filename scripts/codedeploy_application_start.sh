ec5b3b7d9039747393419e521c738dc8627f2cce
bhupender <bhupender.cool98@gmail.com>
2024-01-05 13:23:16 +0530

    fix

--- scripts/codedeploy_application_start.sh
+++ scripts/codedeploy_application_start.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-# Stop all servers and start the server
+
+# Set up NVM environment
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion
+
+# Navigate to the application directory
 cd /home/ec2-user/nest-demo
+
+# Install Node.js dependencies and start the server
 npm install
 npm run build
 npm run start:prod
