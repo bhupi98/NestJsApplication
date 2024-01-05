ddbdb0a007c2e3fd1e365f47ba5ccceadcb80e90
bhupender <bhupender.cool98@gmail.com>
2024-01-05 11:18:27 +0530

    adding the files

--- scripts/codedeploy_after_install.sh
+++ scripts/codedeploy_after_install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-mkdir /home/ubuntu/nest-demo
+sudo chmod -R 777 /home/ubuntu/nest-demo
+
 cd /home/ubuntu/nest-demo
-sudo npm install
+
+#add npm and node to path
+export NVM_DIR="$HOME/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
--- scripts/codedeploy_before_install.sh
+++ scripts/codedeploy_before_install.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-# Install node.js and PM2 globally
-sudo apt-get update
-sudo apt-get install nodejs -y
-sudo apt-get install npm  -y
-sudo npm install pm2 -g	
+
+#download node and npm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+
+# install pm2 globally
+sudo npm install pm2 -g
+
+#create our working directory if it doesnt exist
+DIR="/home/ubuntu/nest-demo"
+if [ -d "$DIR" ]; then
+  echo "${DIR} exists"
+else
+  echo "Creating ${DIR} directory"
+  mkdir ${DIR}
+fi
