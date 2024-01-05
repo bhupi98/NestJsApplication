014c661a1a20c11bcb19fe387427ed2d48c6aa16
bhupender <bhupender.cool98@gmail.com>
2024-01-05 13:11:32 +0530

    demo

--- scripts/codedeploy_application_start.sh
+++ scripts/codedeploy_application_start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # Stop all servers and start the server
-pm2 stop all
-pm2 start /home/ec2-user/nest-demo/dist/main.js --name my-nest-app
+cd /home/ec2-user/nest-demo
+npm install
+npm run build
+npm run start:prod
